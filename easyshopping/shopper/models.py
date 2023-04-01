5ab1dd7e38b4a6ad5395d9bdb03965f1041e75d5
Chimchimster <jorddyishere@gmail.com>
2023-04-01 15:39:01 +0600

    added initial models

--- easyshopping/shopper/models.py
+++ easyshopping/shopper/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+class User(AbstractUser):
+    """ Abstract model for each user """
+    pass
+
+class Good:
+    """ Model which represents each item in shop """
+
+    title = models.CharField(
+        max_length=255,
+        null=False,
+        blank=True,
+        verbose_name='Название товара',
+    )
+    price = models.IntegerField(
+        default=0,
+        verbose_name='Цена товара',
+        null=False,
+        blank=True,
+        validators=[MinValueValidator(10), MaxValueValidator(999999)],
+    )
+
+    description = models.TextField(
+        null=False,
+        blank=True,
+        verbose_name='Описание',
+    )
+
+    image = models.ImageField(
+        upload_to='/images',
+        null=False,
+        blank=True,
+    )
+
+    availability = models.BooleanField(
+        default=False,
+    )
--- easyshopping/shopper/views.py
+++ easyshopping/shopper/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def index(request):
+
+    pass
+
+    return render(request, 'shopper/index.html', {
+        'test': None
+    })
