55ed896611a5e06d4ab1ecbaa4123337332ae373
Chimchimster <jorddyishere@gmail.com>
2023-04-09 15:29:49 +0600

    added initial database-structure

--- easyshopping/shopper/admin.py
+++ easyshopping/shopper/admin.py
@@ -1,7 +1,21 @@
-from .models import User, Good, Shop
+from django.contrib.auth.admin import UserAdmin
+
+from .models import ShopperUser, OrdersStatus, OrderItems, Orders, ProductsDescription, ProductsTypes, Products
+from .forms import UserCreationForm, RegistrationForm
 
 from django.contrib import admin
 
-admin.site.register(User)
-admin.site.register(Good)
-admin.site.register(Shop)
+
+class ShopperUserAdmin(UserAdmin):
+    form = UserCreationForm
+    model = ShopperUser
+    list_display = ['username', 'email']
+
+
+admin.site.register(ShopperUser, ShopperUserAdmin)
+admin.site.register(OrdersStatus)
+admin.site.register(OrderItems)
+admin.site.register(Orders)
+admin.site.register(ProductsDescription)
+admin.site.register(ProductsTypes)
+admin.site.register(Products)
--- easyshopping/shopper/urls.py
+++ easyshopping/shopper/urls.py
@@ -1,12 +1,7 @@
-from .views import IndexView, ShopView, UserLogin, logout_user, RegisterUser
+
 from django.urls import path
 
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
-    path('shop/<slug:shop_slug>', ShopView.as_view(), name='shop'),
-    path('login', UserLogin.as_view(), name='login'),
-    path('logout', logout_user, name='logout'),
-    path('register', RegisterUser.as_view(), name='register'),
 
 ]
