ebed620e7d7e1d50ab13c9aeb5979a9a218556ba
Chimchimster <jorddyishere@gmail.com>
2023-04-15 19:40:43 +0600

    added intial lazy load functionallity

--- easyshopping/shopper/static/shopper/index.js
+++ easyshopping/shopper/static/shopper/index.js
@@ -1,8 +1,14 @@
-// Start with second product
-let counter = 0;
+// Set slider counter
+let slider_counter = 0;
 
-// Quantity of products to load
-const quantity = 4;
+// Set all products counter
+let product_counter = 0;
+
+// Quantity of slider products to load
+const slider_quantity = 4;
+
+// Quantity of all products to load
+const products_quantity = 2;
 
 // Right and left slides variables
 const right = 1;
@@ -10,50 +16,76 @@ const left = -1;
 
 // When DOM is loaded let's generate sales hits
 document.addEventListener('DOMContentLoaded', function() {
-    load(0)
+    load_slider(0)
 
     slide('slide-right', right)
     slide('slide-left', left)
 
+    load_products()
+
 });
 
-// Load all products
-function load(increment) {
-    // Set start and end of products to load it
-    const start = counter + increment;
-    const end = start + quantity;
-    counter = end - 4;
 
-    // Get products and add it to DOM
+// If scrolled to bottom, render new products
+window.onscroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        load_products();
+    }
+}
+
+// Load all products
+function load_products() {
+
+    // Set start and end of all products to load
+    const start = product_counter;
+    const end = start + products_quantity;
+    product_counter = end + 1;
+
+    // Get all products and add it to DOM
+    fetch(`/all_products?start=${start}&end=${end}`)
+    .then(response => response.json())
+    .then(data => {
+        data.products.forEach((item) => add_product(item, 'product', 'all-products'));
+    })
+}
+
+
+// Load slider's products
+function load_slider(increment) {
+    // Set start and end of slider products to load it
+    const start = slider_counter + increment;
+    const end = start + slider_quantity;
+    slider_counter = end - 4;
+
+    // Get slider products and add it to DOM
     fetch(`/sales_hits?start=${start}&end=${end}`)
     .then(response => response.json())
     .then(data => {
-        data.products.forEach(add_product);
+        data.products.forEach((item) => add_product(item, 'sales-product', 'sales-products'));
         // Hiding and showing buttons automatically
         length = data.length;
-        console.log(length, start, end)
         show_hide_buttons(start, end, length)
     })
 }
 
 // Add unique product to DOM
-function add_product(contents) {
+function add_product(contents, child_div_class_name, parent_div_class_name) {
 
     // Creating element to store values
     const product = document.createElement('div')
 
-    product.className = 'sales-product'
+    product.className = child_div_class_name
     product.innerHTML = `<div><p>${contents.productsdescription__product_name}</p><br><img src="media/${contents.productsdescription__product_images}"></div>`
 
     // Add product to DOM
-    document.getElementById('sales-products').append(product);
+    document.getElementById(parent_div_class_name).append(product);
 }
 
 // Links function to specific button
 function slide(button_id, destination) {
     document.getElementById(button_id).onclick = function() {
         clear_slider('sales-products')
-        load(destination)
+        load_slider(destination)
     }
 }
 
@@ -71,7 +103,7 @@ function clear_slider(element) {
 function show_hide_buttons(start, end, length) {
     const left = document.getElementById('slide-left');
     const right = document.getElementById('slide-right');
-    console.log(left)
+
     if (start === 0) {
         left.style.display = 'none';
     } else {
--- easyshopping/shopper/urls.py
+++ easyshopping/shopper/urls.py
@@ -1,10 +1,11 @@
 from django.templatetags.static import static
 from django.urls import path
-from .views import IndexView, sales_hits
+from .views import IndexView, sales_hits, get_products
 
 urlpatterns = [
     path('',  IndexView.as_view(), name='index'),
 
     # API routes
-    path('sales_hits', sales_hits, name='c'),
+    path('sales_hits', sales_hits),
+    path('all_products', get_products)
 ]
