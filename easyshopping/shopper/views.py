a051b84f32a0dfd6a4df48b57f20eda36965f2e8
Chimchimster <jorddyishere@gmail.com>
2023-04-16 18:15:05 +0600

    unknown files

--- easyshopping/shopper/views.py
+++ easyshopping/shopper/views.py
@@ -1,6 +1,6 @@
 import json
 
-import simplejson as simplejson
+import simplejson
 from django.contrib.auth import logout
 from django.core.serializers import serialize
 from django.core.serializers.json import DjangoJSONEncoder
@@ -73,7 +73,7 @@ def response_api(func):
 
 @response_api
 def sales_hits(request):
-    """ Function responsible for section 'sales hits' """
+    """ Function responsible for section 'sales hits'. """
 
     query = Products.objects.select_related().values(
             'productsdescription__product_name',
@@ -85,14 +85,12 @@ def sales_hits(request):
 
 @response_api
 def get_products(request):
-    """ Function responsible for section 'all products' """
+    """ Function responsible for section 'all products'. """
 
     query = Products.objects.select_related().values(
         'productsdescription__product_name',
         'productsdescription__product_images',
     ).distinct()
-
-    print(query)
 
     return query
 
